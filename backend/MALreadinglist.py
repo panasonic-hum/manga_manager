1eb9bc7ea795aeae6dd828bceb44dc00e6917375
swampcreaturee <96924530+swampcreaturee@users.noreply.github.com>
2024-04-29 17:53:49 -0500

    added on hold to mal reading list

--- backend/MALreadinglist.py
+++ backend/MALreadinglist.py
@@ -14,19 +14,24 @@ MAL_user = os.getenv('MAL_USER')
 CR_URL = f"https://myanimelist.net/mangalist/{MAL_user}/load.json?status=1&offset=0"
 COMPL_URL = f"https://myanimelist.net/mangalist/{MAL_user}/load.json?status=2&offset=0"
 PTR_URL = f"https://myanimelist.net/mangalist/{MAL_user}/load.json?status=6&offset=0"
+HOLD_URL = f"https://myanimelist.net/mangalist/{MAL_user}/load.json?status=3&offset=0"
 
 #Alternatively, you can uncomment this part and put your username in the MAL_user variable
+#Also comment out the previous block of code
 
 # MAL_user = "username"
 # CR_URL = f"https://myanimelist.net/mangalist/{MAL_user}/load.json?status=1&offset=0"
 # COMPL_URL = f"https://myanimelist.net/mangalist/{MAL_user}/load.json?status=2&offset=0"
 # PTR_URL = f"https://myanimelist.net/mangalist/{MAL_user}/load.json?status=6&offset=0"
+# HOLD_URL = f"https://myanimelist.net/mangalist/{MAL_user}/load.json?status=3&offset=0"
 
 def main():
     print(MAL_user)
+    os.makedirs("MALlists", exist_ok = True)
     curr_read()
     completed()
     ptr()
+    hold()
 
 def curr_read():
     curr_read = requests.get(CR_URL)
@@ -44,6 +49,12 @@ def ptr():
     ptr = requests.get(PTR_URL)
     data = ptr.json()
     with open("MALlists/ptr.json","w") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+def hold():
+    hold = requests.get(HOLD_URL)
+    data = hold.json()
+    with open("MALlists/on_hold.json","w") as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
